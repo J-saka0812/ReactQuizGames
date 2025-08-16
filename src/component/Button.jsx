dca48c09317e69d6ea882e42696f1e90fabb5c9b
J-saka0812 <J.saka0812@gmail.com>
2025-08-16 18:53:25 +0900

    1st_commit

--- src/component/Button.jsx
+++ src/component/Button.jsx
@@ -1,8 +1,9 @@
 
 
-export default function Button({ children }) {
+export default function Button({ children, onClick }) { 
+    
     return (
-        <button type="button" className='text-xl py-2 px-4 bg-white my-2 w-[8rem] border-2 border-black'>
+        <button type="button" className='text-xl py-2 px-4 bg-white my-2 w-[8rem] border-2 border-black' onClick={onClick}>
             <span className='text-black '>
                 {children}
             </span>
--- src/pages/QuizPage.jsx
+++ src/pages/QuizPage.jsx
@@ -1,20 +1,36 @@
+import { useState } from "react";
 import Button from "../component/Button";
 import Display from "../component/Display"
 import quizData from "../data/quiz"
 
 export default function QuizPage() {
 
-    const quizIndex = 0;
+    const [quizIndex, setQuizIndex] = useState(0);
+    const [answerLogs, setAnswerLogs] = useState([]);
+    const navigation = useNavigate();
+
+    const handleClick = (clickIndex) => {
+        if(clickIndex === quizData[quizIndex].answerIndex){
+            setAnswerLogs((prev) => [...prev, true]);
+        } else {
+            setAnswerLogs((prev) => [...prev, false]);
+        }
+        setQuizIndex((prev) => prev + 1);
+    };
+
     return (
         <>
             <Display>
                 {`Q1. ${quizData[quizIndex].question}`}
             </Display>
-                {
-                    quizData[quizIndex].options.map((item, index) => {
-                        return <Button key={`item-${index}`}>{item}</Button>
-                    })
-                }
+            {
+                quizData[quizIndex].options.map((item, index) => {
+                    return ( <Button key={`item-${index}`} onClick={() => handleClick(index)}>
+                        {item}
+                    </Button>
+                    )
+                })
+            }
         </>
     )
 }
